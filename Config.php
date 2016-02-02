2b2af0a5ab3f8e6167a2fd68699ec6dd218183b1
Hung Nguyen <tinapc@gmail.com>
2016-02-03 06:24:42 +0700

    No message

---
+++ Config.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: hungnguyen
+ * Date: 02/03/16
+ * Time: 6:20 AM
+ */
+
+const CLIENT_ID = '81678134426-l5mn5976kle6e69j7ffgsd0pd68rvrlf.apps.googleusercontent.com';
+const CLIENT_SECRET = '8_NYYvtjpxfS6EEF5QavYHCR';
+const REDIRECT_URL = 'http://localhost/facebook-googlesheet/auth2.php';
