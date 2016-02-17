76a3c157248da5b0f45a96d46e1e09180e64e9a2
Gary-Yau Chan <gary-yauchan@Gary-Yaus-Air.home>
2016-02-17 15:00:40 -0500

    remove id info

--- Config.php
+++ Config.php
@@ -8,13 +8,13 @@
 
 
 // Get those values from https://console.developers.google.com
-const SERVICE_CLIENT_ID = '112883231552734272086';
-const SERVICE_EMAIL = 'my-webservice-2@api-project-81678134426.iam.gserviceaccount.com';
+const SERVICE_CLIENT_ID = '';
+const SERVICE_EMAIL = '';
 //$path_to_p12_file = $rootPath .'service_api.p12';
 
 // Facebook API
-const FACEBOOK_API_KEY = '872365952794421'; // Change to your api key
-const FACEBOOK_API_SECRET = 'ffc6b9458fc8139a7b30b53e8d567547'; // Change to your api secret
+const FACEBOOK_API_KEY = ''; // Change to your api key
+const FACEBOOK_API_SECRET = ''; // Change to your api secret
 const FACEBOOK_CALLBACK = 'http://localhost/facebook-export-into-googlesheet/fb-callback.php'; // Change to your domain
 const DOMAIN = "http://localhost/facebook-export-into-googlesheet/";
 
